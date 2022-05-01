bc008734fae5e0e1e47714044132e2f0199a8910
Aaron Noe <aarminoe@gmail.com>
2022-05-01 11:51:24 -0500

    commit tree 8aa7260e5f8fa6f94a66f007586e9a69705498d7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const input = document.querySelector('input#searchByID')
+    console.log(input.value)
+    if (input.value < 4 && input.value > 0) {
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+          console.log(data)})}
+    else {
+        console.log('Not A Valid Movie')
+    }
+      }
+)}
+
 
 document.addEventListener('DOMContentLoaded', init);
